485c6fdf8d29a53965f7867a016aa73bd9dd7063
gamesbyaisha <82407553+gamesbyaisha@users.noreply.github.com>
2022-05-05 14:41:46 -0400

    most recent

--- two.js
+++ two.js
@@ -4,3 +4,10 @@
     document.querySelector("#text").style.background = localStorage.getItem("color")
 
 })
+
+document.getElementById('select-all').onclick = function() {
+    var checkboxes = document.getElementsByName('motivations');
+    for (var checkbox of checkboxes) {
+        checkbox.checked = this.checked;
+    }
+}
