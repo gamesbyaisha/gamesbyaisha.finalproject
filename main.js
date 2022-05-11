91d9c28d54844c6b6d2fc97515f64144685a9cc3
gamesbyaisha <82407553+gamesbyaisha@users.noreply.github.com>
2022-05-11 16:41:19 -0400

    latest commit

--- main.js
+++ main.js
@@ -1,22 +1,78 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
+﻿// document.addEventListener("DOMContentLoaded", function () {
 
-    // document.querySelector("#education").addEventListener("keyup", function () {
-    //     let val = this.value;
-    //     localStorage.setItem("education", val)
-    // })
+//     // document.querySelector("#education").addEventListener("keyup", function () {
+//     //     let val = this.value;
+//     //     localStorage.setItem("education", val)
+//     // })
+//     var radios = document.getElementsByName("myRadios"); // list of radio buttons
+//     var val = localStorage.getItem('education');
+//     let radios = document.querySelectorAll('.radio'); 
+//     for (i = 0; i < radios.length; i++) {
+//         if (radios[i].checked) {
+//             let val = this.value;
+//             localStorage.setItem("education", val)
+//             document.querySelector("#education").innerHTML = localStorage.getItem("education")
+//         }
+//         // radios[i].addEventListener('change',function(){
+//         // })
+//     }
+// })
 
-    let radios = document.querySelectorAll('.radio'); 
-    for (i = 0; i < radios.length; i++) {
-        if (radios[i].checked) {
-            let val = this.value;
-            localStorage.setItem("education", val)
-            document.querySelector("#education").innerHTML = localStorage.getItem("education")
-        }
-        // radios[i].addEventListener('change',function(){
-        // })
-    }
+document.addEventListener("DOMContentLoaded", function () {
+
+var frmGender = document.education.gender;
+var prev = null;
+for (var i = 0; i < frmGender.length; i++) {
+   frmGender[i].addEventListener('change', function () {
+      (prev) ? console.log(prev.value) : null;
+      if (this !== prev) {
+         prev = this;
+      }
+     //  console.log(this.value);
+      document.getElementsByClassName("selectedEducation")[0].innerHTML = this.value;
+      localStorage.setItem("gender", this.value);
+      document.querySelector("#value").innerHTML = localStorage.getItem("gender")
+ 
+   });
+}
 })
 
+document.addEventListener("DOMContentLoaded", function () {
+var frmPersonality = document.personality.myRadios;
+var prev = null;
+for (var i = 0; i < frmPersonality.length; i++) {
+    frmPersonality[i].addEventListener('change', function () {
+      (prev) ? console.log(prev.value) : null;
+      if (this !== prev) {
+         prev = this;
+      }
+      document.getElementsByClassName("selectedPersonality")[0].innerHTML = this.value;
+      localStorage.setItem("myRadios", this.value);
+      document.querySelector("#value").innerHTML = localStorage.getItem("myRadios")
+ 
+   });
+}
+})
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    var frmSalary = document.education.salary;
+    var prev = null;
+    for (var i = 0; i < frmSalary.length; i++) {
+       frmSalary[i].addEventListener('change', function () {
+          (prev) ? console.log(prev.value) : null;
+          if (this !== prev) {
+             prev = this;
+          }
+         //  console.log(this.value);
+          document.getElementsByClassName("selectedSalary")[0].innerHTML = this.value;
+          localStorage.setItem("salary", this.value);
+          document.querySelector("#value").innerHTML = localStorage.getItem("salary")
+     
+       });
+    }
+    })
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -35,6 +91,7 @@ document.addEventListener("DOMContentLoaded", function () {
             localStorage.setItem("adjective", val)
     })
 })  
+
 
 document.addEventListener("DOMContentLoaded", function () {
 
--- two.js
+++ two.js
@@ -3,6 +3,53 @@
     document.querySelector('button').addEventListener("click", generateWords);
 })
 
+document.addEventListener("DOMContentLoaded", function () {
+var usergender = localStorage.getItem("gender");
+   // document.getElementById('name').innerHTML = username;
+   // Below commented out because <span id="gender"> does not exist on page
+   // document.getElementById('gender').innerHTML = usergender;
+
+   if (usergender === 'high school'){
+      document.getElementById('greetz').innerHTML = 'high school';
+   }else if (usergender === 'middle school'){
+      document.getElementById('greetz').innerHTML = 'middle school';
+   }else if (usergender === 'phD'){
+        document.getElementById('greetz').innerHTML = 'phD';
+   }else if (usergender === 'masters'){
+        document.getElementById('greetz').innerHTML = 'masters';
+   }else if (usergender === 'college'){
+        document.getElementById('greetz').innerHTML = 'college';
+   }else{
+      document.getElementById('greetz').innerHTML = 'unknown';
+   }
+})
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var userpersonality = localStorage.getItem("expectations");
+   
+       if (userpersonality === 'introvert'){
+          document.getElementById('personality').innerHTML = 'introvert';
+       }else{
+          document.getElementById('personality').innerHTML = 'extrovert';
+       }
+    })
+
+document.addEventListener("DOMContentLoaded", function () {
+        var usersalary = localStorage.getItem("expectations");
+       
+            if (usersalary === '$20k'){
+              document.getElementById('salary').innerHTML = '$20k';
+            }if (usersalary === '$50k'){
+                document.getElementById('salary').innerHTML = '$50k';
+            }if (usersalary === '$75k'){
+                document.getElementById('salary').innerHTML = '$75k';
+            }if (usersalary === '$100k'){
+                document.getElementById('salary').innerHTML = '$100k';
+            }else{
+              document.getElementById('salary').innerHTML = '$150k';
+        }
+})
 
 function generateWords() {
     let rm = RiTa.markov(2);
@@ -20,8 +67,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    document.querySelector("#education").innerHTML = localStorage.getItem("education")
-    // document.querySelector("#text").style.background = localStorage.getItem("education")
+    document.querySelector("#motivations").addEventListener("keyup", function () {
+        let val = this.value;
+        localStorage.setItem("motivations", val)
+    })
+})  
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.querySelector("#motivations").innerHTML = localStorage.getItem("motivations")
+    // document.querySelector("#text").style.background = localStorage.getItem("name")
 
 })
 
@@ -60,9 +114,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
 })
 
-document.getElementById('select-all').onclick = function() {
-    var checkboxes = document.getElementsByName('motivations');
-    for (var checkbox of checkboxes) {
-        checkbox.checked = this.checked;
-    }
-}
+// document.getElementById('select-all').onclick = function() {
+//     var checkboxes = document.getElementsByName('motivations');
+//     for (var checkbox of checkboxes) {
+//         checkbox.checked = this.checked;
+//     }
+// }
